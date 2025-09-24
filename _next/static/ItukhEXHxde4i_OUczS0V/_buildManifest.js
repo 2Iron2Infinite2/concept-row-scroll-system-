bd29ff3a955c941fbbb7645073aea0298f098b6e
Jahan Bhoj <jahan.b@eccentricengine.net>
2025-09-24 15:13:31 +0530

    Updates

---
+++ _next/static/ItukhEXHxde4i_OUczS0V/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/02222c90288e0f9d.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/concept-row-scroll-system-//_next/:path+",
+        "destination": "/concept-row-scroll-system-/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
